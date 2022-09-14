90bcd72d30d482dc29245dd9d9aa537237a8d233
Darya Ryzhakova <rizhakovadasha@yandex.ru>
2022-09-14 14:36:01 +0700

    Created BottomSheetBody and notification widget

---
+++ lib/presentation/screens/atm_inside_screen/bottom_sheet_widget/bottom_sheet_body.dart
@@ -0,0 +1,46 @@
+import 'package:atm_test/presentation/screens/atm_inside_screen/bottom_sheet_widget/widgets/custom_notification.dart';
+import 'package:atm_test/presentation/styles/color_styles.dart';
+import 'package:atm_test/presentation/styles/strings.dart';
+import 'package:atm_test/presentation/widgets/custom_button.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class BottomSheetBody extends StatefulWidget {
+  const BottomSheetBody({Key? key}) : super(key: key);
+
+  @override
+  State<BottomSheetBody> createState() => _BottomSheetBodyState();
+}
+
+class _BottomSheetBodyState extends State<BottomSheetBody> {
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 635.h,
+      child: Stack(
+        children: [
+          SingleChildScrollView(
+            physics: const BouncingScrollPhysics(),
+            child: Column(
+              children: const [
+                CustomNotification(),
+              ],
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: CustomButton(
+              width: double.maxFinite,
+              height: 56.h,
+              backgroundColor: ColorStyles.tmnBlue,
+              onTap: () {},
+              text: Strings.saveChanges,
+              textColor: ColorStyles.tmnWhite,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
