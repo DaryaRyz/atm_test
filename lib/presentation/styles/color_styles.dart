45d0c925d3ab2084a65737102e79e0e6b4ad476e
Darya Ryzhakova <rizhakovadasha@yandex.ru>
2022-09-14 10:04:09 +0700

    Initial commit

---
+++ lib/presentation/styles/color_styles.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+abstract class ColorStyles{
+  static const tmnBlue = Color.fromRGBO(33, 140, 239, 1);
+  static const tmnDarkBlue = Color.fromRGBO(19, 16, 55, 1);
+  static const tmnLightBlue = Color.fromRGBO(217, 226, 241, 1);
+  static const tmnRed = Color.fromRGBO(249, 120, 121, 1);
+  static const tmnWhite = Color.fromRGBO(255, 255, 255, 1);
+  static const tmnLightGrey = Color.fromRGBO(204, 204, 204, 1);
+  static const tmnMidGrey = Color.fromRGBO(153, 153, 153, 1);
+  static const tmnUltralightBlue = Color.fromRGBO(241, 241, 249, 1);
+  static const tmnGreen = Color.fromRGBO(0, 190, 8, 1);
+
+  ///atm card
+  static const atmShadow = Color.fromRGBO(0, 91, 169, 0.04);
+}
---
+++ lib/presentation/styles/strings.dart
@@ -0,0 +1,43 @@
+abstract class Strings {
+  static const atmInsideTitle = 'Торговые автоматы';
+
+  ///atm information
+  static const atmWorkingStatus = 'Работает';
+  static const atmNonWorkingStatus = 'Не работает';
+  static const atmBusTypeTitle = 'Тип шины';
+  static const atmSignalLevelTitle = 'Уровень сигнала';
+  static const atmIdTitle = 'Идентификатор';
+  static const atmModemTitle = 'Модем';
+
+  ///loading block
+  static const atmCurrentLoadingLevelTitle = 'Текущий уровень загрузки';
+  static const atmLoadingTitle = 'Загрузка';
+  static const atmInventoryTitle = 'Инвентаризация';
+
+  ///event block
+  static const atmEventTitle = 'События';
+  static const atmEventButtonTitle = 'Смотреть еще';
+
+  ///finance block
+  static const atmFinanceTitle = 'Финансы';
+  static const atmFinanceCash = 'Деньги в ТА';
+  static const atmFinanceCashBack = 'Сдача';
+
+  ///bottom sheet
+  static const notificationText = 'Изменения от 12.07.2021, 12:30 применены успешно';
+  static const decimalPosition = 'Decimal position';
+  static const leftInputHintText = 'Cash';
+  static const rightInputHintText = 'Cashless';
+  static const scaleFactor = 'Scale factor';
+  static const onSound = 'Включить звук';
+  static const masterMode = 'Режим мастер';
+  static const usageMode = 'Используется';
+  static const nonUsageMode = 'Не используется';
+  static const priceLists = 'Прайс листы';
+  static const synchronizingTitle = 'Последняя синхронизация с модемом';
+  static const synchronizingTime = '1 час назад';
+  static const hash = '#';
+  static const price = 'Цена';
+  static const addLine = 'Добавить строку';
+  static const saveChanges = 'Сохранить изменения';
+}
