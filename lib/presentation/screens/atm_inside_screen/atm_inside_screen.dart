e9d9240746142fdc7c3e93cea6c0d86d5d2efeba
Darya Ryzhakova <rizhakovadasha@yandex.ru>
2022-09-14 10:25:31 +0700

    Created LoadingWidget and custom button

--- lib/presentation/screens/atm_inside_screen/atm_inside_screen.dart
+++ lib/presentation/screens/atm_inside_screen/atm_inside_screen.dart
@@ -1,6 +1,7 @@
 import 'package:atm_test/data/repository/atm_repository.dart';
 import 'package:atm_test/domain/bloc/atmBloc.dart';
 import 'package:atm_test/domain/models/atm_model.dart';
+import 'package:atm_test/presentation/screens/atm_inside_screen/loading_widget/loading_widget.dart';
 import 'package:atm_test/presentation/screens/atm_inside_screen/main_info_widget/main_info_widget.dart';
 import 'package:atm_test/presentation/screens/widgets/custom_app_bar.dart';
 import 'package:atm_test/presentation/screens/widgets/custom_error_body.dart';
@@ -84,6 +85,7 @@ class _AtmInsideBody extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 AtmMainInfoWidget(atm: atm),
+                LoadingWidget(fullnessLevel: atm.fullnessLevel),
               ],
             ),
           ),
