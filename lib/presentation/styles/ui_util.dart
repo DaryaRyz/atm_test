24261f5cb141dccf096e6dffb3f214ffd6812066
Darya Ryzhakova <rizhakovadasha@yandex.ru>
2022-09-17 12:40:04 +0700

    fixed notification time bug

--- lib/presentation/styles/ui_util.dart
+++ lib/presentation/styles/ui_util.dart
@@ -31,9 +31,11 @@ abstract class UiUtil {
   }
 
   static fromSuccessSaveTime(DateTime time) {
-    String result = 'Изменения от ${time.day}.';
-    result += time.month < 10 ? '0${time.month}.' : '${time.month.toString()}.';
-    result += '${time.year}, ${time.hour}:';
+    String result = 'Изменения от ';
+    result += time.day < 10 ? '0${time.day}.' : '${time.day}.';
+    result += time.month < 10 ? '0${time.month}.' : '${time.month}.';
+    result += '${time.year}, ';
+    result += time.hour < 10 ? '0${time.hour}:' : '${time.hour}:';
     result += time.minute < 10 ? '0${time.minute}' : '${time.minute}';
     result += '\nприменены успешно';
     return result;
